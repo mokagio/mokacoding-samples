d9d857f274dd450cea36753f5fbc955eb4c8edf2
Gio <giovanni.lodi42@gmail.com>
2014-03-26 21:45:25 +0000

    Step 1: Project setup with old style Podfile

---
+++ PodfileUpgrade/PodfileUpgradeTests/PodfileUpgradeTests.m
@@ -0,0 +1,34 @@
+//
+//  PodfileUpgradeTests.m
+//  PodfileUpgradeTests
+//
+//  Created by Gio on 26/03/2014.
+//  Copyright (c) 2014 mokagio. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PodfileUpgradeTests : XCTestCase
+
+@end
+
+@implementation PodfileUpgradeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
